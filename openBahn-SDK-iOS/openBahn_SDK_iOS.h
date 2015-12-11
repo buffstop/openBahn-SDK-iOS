286a424d9b34565d007bdd785f328320289abcdf
Andreas Buff <autobuff@gmx.de>
2015-12-11 17:08:51 +0100

    initial commit

---
+++ openBahn-SDK-iOS/openBahn_SDK_iOS.h
@@ -0,0 +1,13 @@
+//
+//  openBahn_SDK_iOS.h
+//  openBahn-SDK-iOS
+//
+//  Created by Andreas Buff on 11/12/15.
+//  Copyright (c) 2015 Andreas Buff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface openBahn_SDK_iOS : NSObject
+
+@end
---
+++ openBahn-SDK-iOS/openBahn_SDK_iOS.m
@@ -0,0 +1,13 @@
+//
+//  openBahn_SDK_iOS.m
+//  openBahn-SDK-iOS
+//
+//  Created by Andreas Buff on 11/12/15.
+//  Copyright (c) 2015 Andreas Buff. All rights reserved.
+//
+
+#import "openBahn_SDK_iOS.h"
+
+@implementation openBahn_SDK_iOS
+
+@end
